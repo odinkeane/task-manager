c47d606c6a39f040a45be5a621acb16bd7b14c9b
keane44 <kin.andrew@mail.ru>
2026-02-27 15:30:23 +0700

    Добавлена функция для получения задач по id

---
+++ app.js
@@ -0,0 +1,5 @@
+import { handleCommand } from "./src/commandHandler.js";
+
+handleCommand("add [Задача1] [Описание1] [todo] [2]")
+handleCommand("add [Задача2] [Описание2] [in-progress]")
+handleCommand("show [1]")
--- src/commandHandler.js
+++ src/commandHandler.js
@@ -6,9 +6,11 @@ export function handleCommand(command) {
     try {
         if (command.startsWith("add ")) {
             addTask(command.slice(4))
-            return
+        } else if (command.startsWith("show ")) {
+            showTask(command.slice("5"))
+        } else {
+            throw new Error("Неверная команда!")
         }
-        throw new Error("Неверная команда!")
     } catch (error) {
         console.log(error.message)
     }
@@ -21,6 +23,18 @@ function addTask(data) {
     }
     taskService.addTask(...data)
 }
+
+function showTask(data) {
+    data = parsingString(data)
+    if (data.length != 1) {
+        throw new Error("Неверное количество данных при отображении задачи")
+    }
+
+    const task = taskService.getTaskById(+data[0])
+    if (!task) throw new Error("Такой задачи не существует!")
+    console.log(task.toString())
+}
+
 
 function parsingString(data) {
     return data.match(/\[(.*?)\]/g).map(item => item.slice(1, -1))
